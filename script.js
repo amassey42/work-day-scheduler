9ce38119db44336ecbb400d3fbcd421888619971
andrew <ammassey42@gmail.com>
2022-10-05 14:37:41 -0700

    timer now updates every second

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-var date = moment().format("MMM Do YYYY")
-$("#currentDay").text(date)
-console.log(date)
+//date and time
+function displayTime(){
+var dateTime = moment().format("MMM Do YYYY hh:mm:ss A")
+$("#currentDay").text(dateTime)
+console.log(dateTime)
+}
+
+setInterval(displayTime, 1000)
